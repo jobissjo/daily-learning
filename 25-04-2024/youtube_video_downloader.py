71956e2b3e2040c7d72252af009b04196f891bf3
Jobi S S <jobisjobi1234@gmail.com>
2024-04-26 23:18:12 +0530

    today learned things

--- 25-04-2024/youtube_video_downloader.py
+++ 25-04-2024/youtube_video_downloader.py
@@ -1,14 +1,30 @@
 from pytube import YouTube
+from pytube.exceptions import PytubeError
+import os
 
-entered_url = input("Enter the url you want to download..")
+entered_url = input("Enter the URL you want to download: ")
 yt = YouTube(entered_url)
 
-print("Available Videos with Audio")
 streams_with_audio = yt.streams.filter(file_extension='mp4', audio_codec='mp4a.40.2')
-
+print("Available Videos with Audio:")
 for st in streams_with_audio:
     print(st)
 
-tag_id = int(input("\nEnter the id, you want to download: "))
+tag_id = int(input("\nEnter the id of the stream you want to download: "))
 stream = yt.streams.get_by_itag(tag_id)
-stream.download()
+
+download_path = "G:/Jobi Development/Videos"  # Default download path
+if int(input("Do you want to specify a custom path? (1 for yes, 0 for no): ")):
+    download_path = input("Enter the path where you want to save the video (e.g., /path/to/save): ")
+
+try:
+    stream.download(output_path=download_path)
+    print("Download completed successfully!")
+except PytubeError as e:
+    print(f"An error occurred: {e}")
+    print("Retrying the download...")
+    # Retry the download
+    stream.download()
+
+# https://www.youtube.com/live/2v1lOWe6Uf0?si=Pzv4C-VWRUt81Gsn
+# https://youtu.be/uaV05FSOY14?si=Uw7j0pnJVIWL6Onb
