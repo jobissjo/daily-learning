f48038a5a8201f170acaa09e3aca574456906bed
Jobi S S <jobisjobi1234@gmail.com>
2024-04-30 23:58:46 +0530

    role base login registration completed

---
+++ 30-04-2024/library/authentication/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+from rest_framework.authtoken.models import Token
+
+# Create your models here.
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+       ('admin', "Admin"),
+       ('user', 'User')
+    )
+
+    role = models.CharField(max_length=15, choices=ROLE_CHOICES)
